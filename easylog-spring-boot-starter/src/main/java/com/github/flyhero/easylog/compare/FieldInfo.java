79ab16477a8f06d77347f8ca72f3a0dc93f3591a
flyhero <qfwang666@163.com>
2022-02-27 23:24:34 +0800

    style: 注释字段

--- easylog-spring-boot-starter/src/main/java/com/github/flyhero/easylog/compare/FieldInfo.java
+++ easylog-spring-boot-starter/src/main/java/com/github/flyhero/easylog/compare/FieldInfo.java
@@ -3,20 +3,35 @@ package com.github.flyhero.easylog.compare;
 import lombok.Data;
 
 /**
- * @author WangQingFei(qfwang666@163.com)
+ * @author WangQingFei(qfwang666 @ 163.com)
  * @date 2022/2/20 22:52
  */
 
 @Data
 public class FieldInfo {
 
+    /**
+     * 属性名
+     */
     private String fieldName;
 
+    /**
+     * 老的属性类型
+     */
     private Class<?> oldFieldType;
 
+    /**
+     * 新的属性类型
+     */
     private Class<?> newFieldType;
 
+    /**
+     * 老的属性值
+     */
     private Object oldFieldVal;
 
+    /**
+     * 新的属性值
+     */
     private Object newFieldVal;
 }
