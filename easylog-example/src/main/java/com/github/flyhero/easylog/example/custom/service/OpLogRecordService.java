f8c94a3564412283b71b4dd78755e4b7f9c7cc92
flyhero <qfwang666@163.com>
2022-02-28 22:20:57 +0800

    test: 增加获取日志的实现

---
+++ easylog-example/src/main/java/com/github/flyhero/easylog/example/custom/service/OpLogRecordService.java
@@ -0,0 +1,22 @@
+package com.github.flyhero.easylog.example.custom.service;
+
+import com.github.flyhero.easylog.model.EasyLogInfo;
+import com.github.flyhero.easylog.service.ILogRecordService;
+import com.github.flyhero.easylog.util.JsonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 接收操作日志，可根据情况存储到数据库或发送到MQ
+ *
+ * @author WangQingFei(qfwang666 @ 163.com)
+ * @date 2022/2/28 18:38
+ */
+@Slf4j
+@Service
+public class OpLogRecordService implements ILogRecordService {
+    @Override
+    public void record(EasyLogInfo easyLogInfo) {
+        log.info("hello easy-log:{}", JsonUtils.toJSONString(easyLogInfo));
+    }
+}
