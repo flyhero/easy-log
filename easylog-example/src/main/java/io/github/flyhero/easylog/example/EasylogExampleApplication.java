96c942fb754582376c2f206d8d38cf67aacb26ae
flyhero <qfwang666@163.com>
2022-12-05 20:46:28 +0800

    docs: 说明注解使用场景

--- easylog-example/src/main/java/io/github/flyhero/easylog/example/EasylogExampleApplication.java
+++ easylog-example/src/main/java/io/github/flyhero/easylog/example/EasylogExampleApplication.java
@@ -2,7 +2,9 @@ package io.github.flyhero.easylog.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+// 此注解在 同类中内部方法调用时添加， 项目README中有说明
+//@EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
 public class EasylogExampleApplication {
 
